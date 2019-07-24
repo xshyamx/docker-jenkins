b01e53b3261bbcd0e3e358e9860cfdcd195661b7
shyam <shyamsundar.shanmugam@wipro.com>
2019-07-24 17:13:43 +0530

    adding checksum file and deleting env file regardless whether docker containers were cleaned up

--- remove-nodes.sh
+++ remove-nodes.sh
@@ -4,9 +4,8 @@ for node_env in *.env; do
   node=$(basename $node_env .env)
   # stop container
   if docker stop $node ; then
-    if docker rm $node ; then
-      ## remove node
-      rm -f $node_env
-    fi
+    docker rm $node
   fi
+  ## remove node
+  rm -f $node_env
 done
