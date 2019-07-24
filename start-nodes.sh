a6bb59ba7454cd2a4359684a58046aba354af484
shyam <shyamsundar.shanmugam@wipro.com>
2019-07-24 16:44:17 +0530

    changes to use swarm plugin

--- start-nodes.sh
+++ start-nodes.sh
@@ -8,8 +8,21 @@ EOF
 fi
 n=$1
 
+JENKINS_USER=jenkins
+JENKINS_PASSWORD=jenkins
+JENKINS_HOME=/var/jenkins_home
+
 for i in $(seq $n); do
   node_name=$(printf "node-%02d" $i)
-  ./add-node.sh $node_name
+  cat <<EOF > $NODE_NAME.env
+JENKINS_URL=${JENKINS_URL}
+JENKINS_USER=${JENKINS_USER}
+JENKINS_PASSWORD=${JENKINS_PASSWORD}
+NODE_NAME=${node_name}
+http_proxy=http://10.0.2.2:3128
+https_proxy=http://10.0.2.2:3128
+no_proxy=localhost
+JAVA_OPTS=-Dhttp.proxyHost=10.0.2.2 -Dhttp.proxyPort=3128 -Dhttps.proxyHost=10.0.2.2 -Dhttps.proxyPort=3128
+EOF
   docker run --name $node_name -d --env-file ./${node_name}.env jenkins/node
 done
